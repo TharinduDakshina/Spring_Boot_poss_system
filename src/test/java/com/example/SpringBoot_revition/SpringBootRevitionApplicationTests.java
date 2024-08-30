bc3f9fc0c54bba1dad8e034c8633154ce52ed1f7
TharinduDakshina <tharindudakshina527@gmail.com>
2024-08-30 14:11:25 +0530

    init repository

---
+++ src/test/java/com/example/SpringBoot_revition/SpringBootRevitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_revition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRevitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
